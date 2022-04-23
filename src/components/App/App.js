4695490a87b9713c18003daa8d3dd39b131d1f0b
renkomba <61875726+renkomba@users.noreply.github.com>
2022-04-23 12:28:04 -0400

    moved [business] to App.js and passed it as prop to <Business /> through <BusinessList />

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
-import logo from '../../logo.svg';
+// import ReactDOM from 'react-dom'
+// import logo from '../../logo.svg';
 import './App.css';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
+
+const business = {
+  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
+
+let businesses = [business, business, business, business, business, business];
 
 function App() {
   return (
     <div className='App'>
       <h1>ravenous</h1>
       <SearchBar />
-      <BusinessList />
+      <BusinessList businesses={businesses} />
     </div>
   );
 }
